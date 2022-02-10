0fb6c69ef46878ef4fe39a14f5c61b8aefd4632a
Cormac Doyle <doylec54@tcd.ie>
2022-02-10 10:36:28 +0000

    plane model

--- main.cpp
+++ main.cpp
@@ -20,7 +20,13 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/euler_angles.hpp>
 
+
+#include "utils/mesh.h"
+#include "utils/shaders.h"
+#include "utils/vbo.h"
+#include "main.h"
 
 
 /*----------------------------------------------------------------------------
@@ -28,243 +34,58 @@ MESH TO LOAD
 ----------------------------------------------------------------------------*/
 // this mesh is a dae file format but you should be able to use any other format too, obj is typically what is used
 // put the mesh in your project directory, or provide a filepath for it here
-#define MESH_NAME "models/simplePlane.dae"
+#define MESH_PLANE "models/simplePlane.dae"
+#define MESH_PROPELLOR "models/propellor.dae"
+
 /*----------------------------------------------------------------------------
 ----------------------------------------------------------------------------*/
 
-#pragma region SimpleTypes
-typedef struct
-{
-	size_t mPointCount;
-	std::vector<glm::vec3> mVertices;
-	std::vector<glm::vec3> mNormals;
-	std::vector<glm::vec2> mTextureCoords;
-} ModelData;
-#pragma endregion SimpleTypes
+
 
 using namespace std;
+
 GLuint shaderProgramID;
 
-ModelData mesh_data;
 unsigned int mesh_vao = 0;
 int width = 800;
 int height = 600;
 
-GLuint loc1, loc2, loc3;
-GLfloat rotate_y = 0.0f;
+GLfloat rotate_propellor = 0.0f;
+float propellor_pos_x = 2.1f;
+
+GLfloat plane_rotate_x = 0.0f;
+GLfloat plane_rotate_y = 0.0f;
+GLfloat plane_rotate_z = 0.0f;
 
 
-#pragma region MESH LOADING
-/*----------------------------------------------------------------------------
-MESH LOADING FUNCTION
-----------------------------------------------------------------------------*/
+ModelData mesh_data;
 
-ModelData load_mesh(const char* file_name) {
-	ModelData modelData;
-	modelData.mPointCount = 0;
-
-	/* Use assimp to read the model file, forcing it to be read as    */
-	/* triangles. The second flag (aiProcess_PreTransformVertices) is */
-	/* relevant if there are multiple meshes in the model file that   */
-	/* are offset from the origin. This is pre-transform them so      */
-	/* they're in the right position.                                 */
-	const aiScene* scene = aiImportFile(
-		file_name, 
-		aiProcess_Triangulate | aiProcess_PreTransformVertices
-	); 
-
-	if (!scene) {
-		fprintf(stderr, "ERROR: reading mesh %s\n", file_name);
-		return modelData;
-	}
-
-	printf("  %i materials\n", scene->mNumMaterials);
-	printf("  %i meshes\n", scene->mNumMeshes);
-	printf("  %i textures\n", scene->mNumTextures);
-
-	for (unsigned int m_i = 0; m_i < scene->mNumMeshes; m_i++) {
-		const aiMesh* mesh = scene->mMeshes[m_i];
-		printf("    %i vertices in mesh\n", mesh->mNumVertices);
-		modelData.mPointCount += mesh->mNumVertices;
-		for (unsigned int v_i = 0; v_i < mesh->mNumVertices; v_i++) {
-			if (mesh->HasPositions()) {
-				const aiVector3D* vp = &(mesh->mVertices[v_i]);
-				modelData.mVertices.push_back(glm::vec3(vp->x, vp->y, vp->z));
-			}
-			if (mesh->HasNormals()) {
-				const aiVector3D* vn = &(mesh->mNormals[v_i]);
-				modelData.mNormals.push_back(glm::vec3(vn->x, vn->y, vn->z));
-			}
-			if (mesh->HasTextureCoords(0)) {
-				const aiVector3D* vt = &(mesh->mTextureCoords[0][v_i]);
-				modelData.mTextureCoords.push_back(glm::vec2(vt->x, vt->y));
-			}
-			if (mesh->HasTangentsAndBitangents()) {
-				/* You can extract tangents and bitangents here              */
-				/* Note that you might need to make Assimp generate this     */
-				/* data for you. Take a look at the flags that aiImportFile  */
-				/* can take.                                                 */
-			}
-		}
-	}
-
-	aiReleaseImport(scene);
-	return modelData;
-}
-
-#pragma endregion MESH LOADING
-
-// Shader Functions- click on + to expand
-#pragma region SHADER_FUNCTIONS
-char* readShaderSource(const char* shaderFile) {
-	FILE* fp;
-	fopen_s(&fp, shaderFile, "rb");
-
-	if (fp == NULL) { return NULL; }
-
-	fseek(fp, 0L, SEEK_END);
-	long size = ftell(fp);
-
-	fseek(fp, 0L, SEEK_SET);
-	char* buf = new char[size + 1];
-	fread(buf, 1, size, fp);
-	buf[size] = '\0';
-
-	fclose(fp);
-
-	return buf;
-}
-
-
-static void AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum ShaderType)
+void loadPropellor(glm::mat4& modelPlane, int matrix_location, float propellor_pos)
 {
-	// create a shader object
-	GLuint ShaderObj = glCreateShader(ShaderType);
+	mesh_data = generateObjectBufferMesh(MESH_PROPELLOR, shaderProgramID);
+	// Set up the child matrix
+	glm::mat4 modelPropellor = glm::mat4(1.0f);
+	modelPropellor = glm::translate(modelPropellor, glm::vec3(propellor_pos, -0.4f, 2.2f));
+	modelPropellor = glm::rotate(modelPropellor, glm::radians(rotate_propellor), glm::vec3(0, 0, 1));
 
-	if (ShaderObj == 0) {
-		std::cerr << "Error creating shader..." << std::endl;
-		std::cerr << "Press enter/return to exit..." << std::endl;
-		std::cin.get();
-		exit(1);
-	}
-	const char* pShaderSource = readShaderSource(pShaderText);
+	// Apply the root matrix to the child matrix
+	modelPropellor = modelPlane * modelPropellor;
 
-	// Bind the source code to the shader, this happens before compilation
-	glShaderSource(ShaderObj, 1, (const GLchar**)&pShaderSource, NULL);
-	// compile the shader and check for errors
-	glCompileShader(ShaderObj);
-	GLint success;
-	// check for shader related errors using glGetShaderiv
-	glGetShaderiv(ShaderObj, GL_COMPILE_STATUS, &success);
-	if (!success) {
-		GLchar InfoLog[1024] = { '\0' };
-		glGetShaderInfoLog(ShaderObj, 1024, NULL, InfoLog);
-		std::cerr << "Error compiling "
-			<< (ShaderType == GL_VERTEX_SHADER ? "vertex" : "fragment")
-			<< " shader program: " << InfoLog << std::endl;
-		std::cerr << "Press enter/return to exit..." << std::endl;
-		std::cin.get();
-		exit(1);
-	}
-	// Attach the compiled shader object to the program object
-	glAttachShader(ShaderProgram, ShaderObj);
+	// Update the appropriate uniform and draw the mesh again
+	glUniformMatrix4fv(matrix_location, 1, GL_FALSE, glm::value_ptr(modelPropellor));
+	glDrawArrays(GL_TRIANGLES, 0, mesh_data.mPointCount);
 }
 
-GLuint CompileShaders()
+void loadPlane(glm::mat4& modelPlane, int matrix_location)
 {
-	//Start the process of setting up our shaders by creating a program ID
-	//Note: we will link all the shaders together into this ID
-	shaderProgramID = glCreateProgram();
-	if (shaderProgramID == 0) {
-		std::cerr << "Error creating shader program..." << std::endl;
-		std::cerr << "Press enter/return to exit..." << std::endl;
-		std::cin.get();
-		exit(1);
-	}
+	mesh_data = generateObjectBufferMesh(MESH_PLANE, shaderProgramID);
 
-	// Create two shader objects, one for the vertex, and one for the fragment shader
-	AddShader(shaderProgramID, "shaders/simpleVertexShader.txt", GL_VERTEX_SHADER);
-	AddShader(shaderProgramID, "shaders/simpleFragmentShader.txt", GL_FRAGMENT_SHADER);
+	modelPlane = glm::mat4(1.0f);
+	modelPlane = glm::yawPitchRoll(glm::radians(plane_rotate_x), glm::radians(plane_rotate_y), glm::radians(plane_rotate_z));
 
-	GLint Success = 0;
-	GLchar ErrorLog[1024] = { '\0' };
-	// After compiling all shader objects and attaching them to the program, we can finally link it
-	glLinkProgram(shaderProgramID);
-	// check for program related errors using glGetProgramiv
-	glGetProgramiv(shaderProgramID, GL_LINK_STATUS, &Success);
-	if (Success == 0) {
-		glGetProgramInfoLog(shaderProgramID, sizeof(ErrorLog), NULL, ErrorLog);
-		std::cerr << "Error linking shader program: " << ErrorLog << std::endl;
-		std::cerr << "Press enter/return to exit..." << std::endl;
-		std::cin.get();
-		exit(1);
-	}
-
-	// program has been successfully linked but needs to be validated to check whether the program can execute given the current pipeline state
-	glValidateProgram(shaderProgramID);
-	// check for program related errors using glGetProgramiv
-	glGetProgramiv(shaderProgramID, GL_VALIDATE_STATUS, &Success);
-	if (!Success) {
-		glGetProgramInfoLog(shaderProgramID, sizeof(ErrorLog), NULL, ErrorLog);
-		std::cerr << "Invalid shader program: " << ErrorLog << std::endl;
-		std::cerr << "Press enter/return to exit..." << std::endl;
-		std::cin.get();
-		exit(1);
-	}
-	// Finally, use the linked shader program
-	// Note: this program will stay in effect for all draw calls until you replace it with another or explicitly disable its use
-	glUseProgram(shaderProgramID);
-	return shaderProgramID;
+	glUniformMatrix4fv(matrix_location, 1, GL_FALSE, glm::value_ptr(modelPlane));
+	glDrawArrays(GL_TRIANGLES, 0, mesh_data.mPointCount);
 }
-#pragma endregion SHADER_FUNCTIONS
-
-// VBO Functions - click on + to expand
-#pragma region VBO_FUNCTIONS
-void generateObjectBufferMesh() {
-	/*----------------------------------------------------------------------------
-	LOAD MESH HERE AND COPY INTO BUFFERS
-	----------------------------------------------------------------------------*/
-
-	//Note: you may get an error "vector subscript out of range" if you are using this code for a mesh that doesnt have positions and normals
-	//Might be an idea to do a check for that before generating and binding the buffer.
-
-	mesh_data = load_mesh(MESH_NAME);
-	unsigned int vp_vbo = 0;
-	loc1 = glGetAttribLocation(shaderProgramID, "vertex_position");
-	loc2 = glGetAttribLocation(shaderProgramID, "vertex_normal");
-	loc3 = glGetAttribLocation(shaderProgramID, "vertex_texture");
-
-	glGenBuffers(1, &vp_vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, vp_vbo);
-	glBufferData(GL_ARRAY_BUFFER, mesh_data.mPointCount * sizeof(glm::vec3), &mesh_data.mVertices[0], GL_STATIC_DRAW);
-	unsigned int vn_vbo = 0;
-	glGenBuffers(1, &vn_vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, vn_vbo);
-	glBufferData(GL_ARRAY_BUFFER, mesh_data.mPointCount * sizeof(glm::vec3), &mesh_data.mNormals[0], GL_STATIC_DRAW);
-
-	//	This is for texture coordinates which you don't currently need, so I have commented it out
-	//	unsigned int vt_vbo = 0;
-	//	glGenBuffers (1, &vt_vbo);
-	//	glBindBuffer (GL_ARRAY_BUFFER, vt_vbo);
-	//	glBufferData (GL_ARRAY_BUFFER, monkey_head_data.mTextureCoords * sizeof (vec2), &monkey_head_data.mTextureCoords[0], GL_STATIC_DRAW);
-
-	unsigned int vao = 0;
-	glBindVertexArray(vao);
-
-	glEnableVertexAttribArray(loc1);
-	glBindBuffer(GL_ARRAY_BUFFER, vp_vbo);
-	glVertexAttribPointer(loc1, 3, GL_FLOAT, GL_FALSE, 0, NULL);
-	glEnableVertexAttribArray(loc2);
-	glBindBuffer(GL_ARRAY_BUFFER, vn_vbo);
-	glVertexAttribPointer(loc2, 3, GL_FLOAT, GL_FALSE, 0, NULL);
-
-	//	This is for texture coordinates which you don't currently need, so I have commented it out
-	//	glEnableVertexAttribArray (loc3);
-	//	glBindBuffer (GL_ARRAY_BUFFER, vt_vbo);
-	//	glVertexAttribPointer (loc3, 2, GL_FLOAT, GL_FALSE, 0, NULL);
-}
-#pragma endregion VBO_FUNCTIONS
-
 
 void display() {
 
@@ -275,6 +96,7 @@ void display() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glUseProgram(shaderProgramID);
 
+	// load mesh into a vertex buffer array
 
 	//Declare your uniform variables that will be used in your shader
 	int matrix_location = glGetUniformLocation(shaderProgramID, "model");
@@ -285,31 +107,24 @@ void display() {
 	// Root of the Hierarchy
 	glm::mat4 view = glm::mat4(1.0f);
 	glm::mat4 persp_proj = glm::perspective(45.0f, (float)width / (float)height, 0.1f, 1000.0f);
-	glm::mat4 model = glm::mat4(1.0f);
-	model = glm::rotate(model, glm::radians(rotate_y), glm::vec3(0,0,1));
+	
 	view = glm::translate(view, glm::vec3(0.0, 0.0, -10.0f));
 
 	// update uniforms & draw
 	glUniformMatrix4fv(proj_mat_location, 1, GL_FALSE, glm::value_ptr(persp_proj));
 	glUniformMatrix4fv(view_mat_location, 1, GL_FALSE, glm::value_ptr(view));
-	glUniformMatrix4fv(matrix_location, 1, GL_FALSE, glm::value_ptr(model));
-	glDrawArrays(GL_TRIANGLES, 0, mesh_data.mPointCount);
 
-	// Set up the child matrix
-	glm::mat4 modelChild = glm::mat4(1.0f);
-	modelChild = glm::rotate(modelChild, glm::radians(180.0f), glm::vec3(0,0,1));
-	modelChild = glm::rotate(modelChild, glm::radians(rotate_y),glm::vec3(0,1,0));
-	modelChild = glm::translate(modelChild, glm::vec3(0.0f, 1.9f, 0.0f));
 
-	// Apply the root matrix to the child matrix
-	modelChild = model * modelChild;
+	glm::mat4 modelPlane;
+	loadPlane(modelPlane, matrix_location);
 
-	// Update the appropriate uniform and draw the mesh again
-	glUniformMatrix4fv(matrix_location, 1, GL_FALSE, glm::value_ptr(modelChild));
-	glDrawArrays(GL_TRIANGLES, 0, mesh_data.mPointCount);
+	loadPropellor(modelPlane, matrix_location, propellor_pos_x);
+	loadPropellor(modelPlane, matrix_location, propellor_pos_x*-1);
+
 
 	glutSwapBuffers();
 }
+
 
 
 void updateScene() {
@@ -322,8 +137,8 @@ void updateScene() {
 	last_time = curr_time;
 
 	// Rotate the model slowly around the y axis at 20 degrees per second
-	rotate_y += 20.0f * delta;
-	rotate_y = fmodf(rotate_y, 360.0f);
+	rotate_propellor += 200.0f * delta;
+	rotate_propellor = fmodf(rotate_propellor, 360.0f);
 
 	// Draw the next frame
 	glutPostRedisplay();
@@ -333,16 +148,30 @@ void updateScene() {
 void init()
 {
 	// Set up the shaders
-	GLuint shaderProgramID = CompileShaders();
-	// load mesh into a vertex buffer array
-	generateObjectBufferMesh();
-
+	shaderProgramID = CompileShaders();
+	
 }
 
 // Placeholder code for the keypress
+float rotate_speed = 10.0f;
 void keypress(unsigned char key, int x, int y) {
-	if (key == 'x') {
-		//Translate the base, etc.
+	if (key == 'y') {
+		plane_rotate_x += rotate_speed;
+	}
+	if (key == '6') {
+		plane_rotate_x -= rotate_speed;
+	}
+	if (key == 'p') {
+		plane_rotate_y += rotate_speed;
+	}
+	if (key == '0') {
+		plane_rotate_y -= rotate_speed;
+	}
+	if (key == 'r') {
+		plane_rotate_z += rotate_speed;
+	}
+	if (key == '4') {
+		plane_rotate_z -= rotate_speed;
 	}
 }
 
---
+++ main.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void loadPlane(glm::mat4& modelPlane, int matrix_location);
+
+void loadPropellor(glm::mat4& modelPlane, int matrix_location);
