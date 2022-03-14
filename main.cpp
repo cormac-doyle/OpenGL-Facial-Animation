12bc9ac115d771d137a0112df4d7781c4faef19a
Cormac Neil Doyle <doylec54@ITSERV.SCSS.TCD.IE>
2022-03-14 18:49:15 +0000

    vertex picker is wonky :(

--- main.cpp
+++ main.cpp
@@ -90,10 +90,12 @@ static float aspect = width/ (float) height;
 
 ModelData mesh_data_neutral_original;
 ModelData mesh_data_neutral;
+std::vector<ModelData> facialExpressions;
+
 
 std::vector < std::vector<glm::vec3> > deltaMs;
 std::vector<float> mWeights;
-ModelData mesh_data_jaw_open;
+ModelData mesh_data;
 
 bool playAnim = false;
 vector<vector<float>> animationWeights;
@@ -134,6 +136,85 @@ void applyDeltaM(ModelData& mesh_data_neutral, std::vector<glm::vec3> deltaM, fl
 
 }
 
+
+glm::vec3 vertexPicker(int x, int y, glm::mat4 VM, glm::mat4 P, std::vector<ModelData> meshes, int& mesh_index, int& vertex_index)
+{
+	glm::vec3 window;
+	window.x = x;
+	window.y = height - y - 1;
+	glReadPixels(x, height - y - 1, 1, 1, GL_DEPTH_COMPONENT, GL_FLOAT, &window.z);
+
+	glm::vec3 object = glm::unProject(window, VM, P, glm::vec4(0.0f, 0.0f, width, height));
+	std::cout << "object coords are " << glm::to_string(object) << endl;
+
+	// find nearest vertex 
+	GLfloat dist = 10;
+	GLfloat temp = 0.0f;
+	GLuint m_index = 0; // index of mesh with closest vertex
+	GLuint v_index = 0; // index of closest vertex
+
+	// brute force through all vertices to find nearest one 
+	for (int i = 0; i < meshes.size(); i++) {
+		for (int j = 0; j < meshes[i].mVertices.size(); j++) {
+
+			temp = glm::distance(object, meshes[i].mVertices[j]);
+			if (temp <= dist) {
+				dist = temp;
+				m_index = i;
+				v_index = j;
+			}
+			
+		}
+	}
+	mesh_index = m_index;
+	vertex_index = v_index;
+	glm::vec3 vertex = meshes[m_index].mVertices[v_index];
+	//constraints.push_back(v_index); //add index of constrained vertex to list of constraints
+	//mo.conservativeResize(constraints.size() * 3);
+	//mo(3 * constraints.size() - 3) = vertex.x;
+	//mo(3 * constraints.size() - 2) vertex.y;
+	//mo(3 * constraints.size() - 1) vertex.z;
+	std::cout << "vertex position: " << glm::to_string(vertex) << std::endl;
+	return vertex;
+
+}
+
+int findClosestExpression(int x, int y, glm::mat4 VM, glm::mat4 P, int chosenVertexIndex, std::vector<ModelData> expressionMeshes) {
+	glm::vec3 window;
+	window.x = x;
+	window.y = height - y - 1;
+	glReadPixels(x, height - y - 1, 1, 1, GL_DEPTH_COMPONENT, GL_FLOAT, &window.z);
+
+	glm::vec3 object = glm::unProject(window, VM, P, glm::vec4(0.0f, 0.0f, width, height));
+	std::cout << "object coords are " << glm::to_string(object) << endl;
+	GLfloat dist = 100000000;
+	GLfloat temp = 0.0f;
+	GLuint m_index = 0; // index of mesh with closest vertex
+	GLuint v_index = 0; // index of closest vertex
+
+	std::cout << "findClosestExpression Debugging "<< std::endl;
+	std::cout << "chosenVertexIndex: " << chosenVertexIndex<<std::endl;
+
+	
+	for (int i = 0; i < expressionMeshes.size(); i++) {
+		
+
+			temp = glm::distance(object, expressionMeshes[i].mVertices[chosenVertexIndex]);
+			std::cout << mesh_file_names[i] <<" distance " << temp << std::endl;
+			if (temp <= dist) {
+				dist = temp;
+				m_index = i;
+				
+			}
+
+		
+	}
+	return m_index;
+}
+
+
+glm::mat4 view;
+glm::mat4 persp_proj;
 void display() {
 	ImGui_ImplOpenGL3_NewFrame();
 	ImGui_ImplGLUT_NewFrame();
@@ -175,8 +256,8 @@ void display() {
 
 
 	// Root of the Hierarchy
-	glm::mat4 view = glm::mat4(1.0f);
-	glm::mat4 persp_proj = glm::perspective(45.0f, (float)width / (float)height, 0.1f, 1000.0f);
+	view = glm::mat4(1.0f);
+	persp_proj = glm::perspective(45.0f, (float)width / (float)height, 0.1f, 1000.0f);
 	
 	view = glm::translate(view, glm::vec3(10.0, -15.0f, -50.0f));
 
@@ -193,7 +274,13 @@ void display() {
 
 
 int frame_num = 0;
+int mouse_x;
+bool mouseClickedDown = false;
+bool mouseClickedUp = false;
 
+glm::vec2 mousePosDown;
+glm::vec2 mousePosUp;
+int chosen_vertex_index = -1;
 void updateScene() {
 
 	static DWORD last_time = 0;
@@ -205,7 +292,34 @@ void updateScene() {
 
 	mesh_data_neutral = mesh_data_neutral_original;
 	
+	
+	if (mouseClickedDown) {
+		std::cout << "MOUSE DOWN X: " << mousePosDown.x << std::endl;
+		std::cout << "MOUSE DOWN Y: " << mousePosDown.y << std::endl;
+		std::vector<ModelData> neutral;
+		neutral.push_back(mesh_data_neutral);
+		int mesh_index = -1;
+		
 
+		glm::vec3 mouseVertex = vertexPicker((int) mousePosDown.x, (int) mousePosDown.y, view, persp_proj, neutral, mesh_index, chosen_vertex_index);
+		std::cout << "mesh index " << mesh_index << std::endl;
+		std::cout << "chosen vertix is: " << glm::to_string(mouseVertex) << std::endl;
+
+		mouseClickedDown = false;
+	}
+	if (mouseClickedUp) {
+		std::cout << "MOUSE UP X: " << mousePosUp.x << std::endl;
+		std::cout << "MOUSE UP Y: " << mousePosUp.y << std::endl;
+		//std::vector<ModelData> fa;
+		//neutral.push_back(mesh_data_neutral);
+		
+
+		int mesh_index =findClosestExpression( (int) mousePosUp.x, (int) mousePosUp.y, view, persp_proj, chosen_vertex_index, facialExpressions);
+		mWeights[mesh_index]=1.0f;
+		std::cout << "mesh index " << mesh_index << std::endl;
+		
+		mouseClickedUp = false;
+	}
 
 	if (playAnim) {
 
@@ -224,22 +338,21 @@ void updateScene() {
 		}
 	}
 
-
-	
 	// Draw the next frame
 	glutPostRedisplay();
 }
 
-void calcDeltaM(const char* MESH)
+//std::vector
+void calcDeltaM(ModelData mesh_data)
 {
-	mesh_data_jaw_open = load_mesh(MESH);
+	
 	
 	std::vector<glm::vec3> deltaM;
 	for (unsigned int i = 0; i < mesh_data_neutral.mPointCount; i++) {
 		glm::vec3 vertice;
-		vertice.x = mesh_data_neutral.mVertices[i].x - mesh_data_jaw_open.mVertices[i].x;
-		vertice.y = mesh_data_neutral.mVertices[i].y - mesh_data_jaw_open.mVertices[i].y;
-		vertice.z = mesh_data_neutral.mVertices[i].z - mesh_data_jaw_open.mVertices[i].z;
+		vertice.x = mesh_data_neutral.mVertices[i].x - mesh_data.mVertices[i].x;
+		vertice.y = mesh_data_neutral.mVertices[i].y - mesh_data.mVertices[i].y;
+		vertice.z = mesh_data_neutral.mVertices[i].z - mesh_data.mVertices[i].z;
 
 		deltaM.push_back(vertice);
 	}
@@ -258,26 +371,30 @@ void read_anim_text_file() {
 	newfile.open("blendshape_animation.txt", ios::in); //open a file to perform read operation using file object
 	if (newfile.is_open()) { //checking whether the file is open
 		string line;
-	while (getline(newfile, line)) { //read data from file object and put it into string.
-		//cout << line << "\n"; //print the data of the string
+		while (getline(newfile, line)) { //read data from file object and put it into string.
+			//cout << line << "\n"; //print the data of the string
 		
-		size_t pos = 0;
-		vector<float> lineWeights;
+			size_t pos = 0;
+			vector<float> lineWeights;
 		
-		//split line by a delimiter found here: https://stackoverflow.com/questions/14265581/parse-split-a-string-in-c-using-string-delimiter-standard-c
-		while ((pos = line.find(delimiter)) != std::string::npos) {
-			std::string weight = line.substr(0, pos);
-			//std::cout << weight << std::endl;
-			lineWeights.push_back(std::stof(weight));
+			//split line by a delimiter found here: https://stackoverflow.com/questions/14265581/parse-split-a-string-in-c-using-string-delimiter-standard-c
+			while ((pos = line.find(delimiter)) != std::string::npos) {
+				std::string weight = line.substr(0, pos);
+				//std::cout << weight << std::endl;
+				lineWeights.push_back(std::stof(weight));
 
-			line.erase(0, pos + delimiter.length());
+				line.erase(0, pos + delimiter.length());
+			}
+			animationWeights.push_back(lineWeights);
+
 		}
-		animationWeights.push_back(lineWeights);
-
-	}
 	newfile.close(); //close the file object.
+	}
 }
-}
+
+//found in power point real-time animation slides
+
+
 
 void init()
 {
@@ -299,7 +416,11 @@ void init()
 	std::cout << "Calculating deltaM vertices..." << std::endl;
 	for (std::string name : mesh_file_names) {
 		std::string filepath = "models/high-res2/" + name;
-		calcDeltaM(filepath.c_str());
+
+		mesh_data = load_mesh(filepath.c_str());
+		facialExpressions.push_back(mesh_data);
+
+		calcDeltaM(mesh_data);
 
 	}
 	std::cout << "Finished loading deltaM vertices." << std::endl;
@@ -310,13 +431,7 @@ void init()
 
 // Placeholder code for the keypress
 float rotate_speed = 10.0f;
-void keypress(unsigned char key, int x, int y) {
 
-	//ImGui_ImplGLUT_KeyboardFunc(key, x, y);
-
-	
-	glutPostRedisplay();
-}
 
 void reshape(GLint w, GLint h) {
 	// imgui reshape func
@@ -327,6 +442,27 @@ void reshape(GLint w, GLint h) {
 }
 
 
+void mouseFunction(int button, int state, int x, int y) {
+	if (x > 490) {
+		if (button == 0 && state == GLUT_DOWN) {
+
+			mousePosDown.x = x;
+			mousePosDown.y = y;
+
+			mouseClickedDown = true;
+		}
+		if (button == 0 && state == GLUT_UP) {
+
+			mousePosUp.x = x;
+			mousePosUp.y = y;
+
+			mouseClickedUp = true;
+		}
+	}
+	
+	ImGui_ImplGLUT_MouseFunc(button,state,x,y);
+}
+
 int main(int argc, char** argv) {
 
 	// Set up the window
@@ -336,12 +472,12 @@ int main(int argc, char** argv) {
 
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_MULTISAMPLE);
 	glutInitWindowSize(width, height);
-	glutCreateWindow("Hello Triangle");
+	glutCreateWindow("Facial Animation");
 
 	// Tell glut where the display function is
 	glutDisplayFunc(display);
 	glutIdleFunc(updateScene);
-	glutKeyboardFunc(keypress);
+	
 	//glutReshapeFunc(reshape);
 	//glutSetOption();
 
@@ -363,6 +499,7 @@ int main(int argc, char** argv) {
 	// Setup Platform/Renderer bindings
 	ImGui_ImplGLUT_Init();
 	ImGui_ImplGLUT_InstallFuncs(); // use the imgui glut funcs
+	glutMouseFunc(mouseFunction);
 	ImGui_ImplOpenGL3_Init();
 
 	// Set up your objects and shaders
@@ -376,4 +513,5 @@ int main(int argc, char** argv) {
 	ImGui::DestroyContext();
 	return 0;
 }
+
  
